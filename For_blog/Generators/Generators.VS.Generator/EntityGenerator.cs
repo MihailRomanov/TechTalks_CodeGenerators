1fc8a820fabd9492c46bfab644837be70783b63a
Mihail Romanov <romanov.m@skbkontur.ru>
2021-01-17 19:44:51 +0400

    Add VS single-file generator

---
+++ For_blog/Generators/Generators.VS.Generator/EntityGenerator.cs
@@ -0,0 +1,35 @@
+﻿using Generators.Common.EntitiesModel;
+using Generators.Implementation.TemplateBased.T4;
+using Generators.ModelReaders;
+using Microsoft.VisualStudio.TextTemplating.VSHost;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Generators.VS.Generator
+{
+    [Guid("132DFB47-2ED7-45BF-BC07-C65003E7F088")]
+    [ProvideCodeGenerator(
+        typeof(EntityGenerator),
+        "EntityGenerator",
+        "Entity DTO Generator",
+        true)]
+    public class EntityGenerator : BaseCodeGenerator
+    {
+        public override string GetDefaultExtension() => ".cs";
+
+        protected override byte[] GenerateCode(string inputFileName, string inputFileContent)
+        {
+            var entities = JsonModelReader.Read(inputFileContent);
+            var model = new Model
+            {
+                Entities = entities,
+                NamespaceName = FileNamespace
+            };
+
+            var generator = new T4Generator();
+            var outContent = generator.Generate(model);
+
+            return Encoding.UTF8.GetBytes(outContent);
+        }
+    }
+}
