3b82a400fdb9ab15ad0ef75f644318355a2caf1e
Mihail Romanov <romanov.m@skbkontur.ru>
2021-01-23 18:40:08 +0400

    Add MSBuild generator support

---
+++ For_blog/Generators/Generators.Build/BaseGeneratorTask.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Generators
+{
+    public abstract class BaseGeneratorTask: Task
+    {
+        public ITaskItem[] ModelFiles { get; set; }
+        public string OutFolder { get; set; }
+        public string RootNamespace { get; set; }
+
+
+        [Output]
+        public ITaskItem[] ResultFiles { get; set; }
+
+        protected abstract string Generate(string modelContent, string namespaceName);
+        protected abstract string OutFileExtension { get; }
+
+        public override bool Execute()
+        {
+            var resultFiles = new List<TaskItem>();
+
+            if (ModelFiles == null || ModelFiles.Length == 0) return true;
+
+            foreach (var infileItem in ModelFiles)
+            {
+                var fileName = Path.GetFileName(infileItem.ItemSpec);
+                var relativePath = infileItem.GetMetadata("RelativeDir");
+                var outFileName = Path.Combine(OutFolder, relativePath, fileName + OutFileExtension);
+                var customToolNamespace = infileItem.GetMetadata("CustomToolNamespace");
+                var namespaceName = GetNamespace(customToolNamespace, relativePath, RootNamespace);
+
+                var outFileFolder = Path.GetDirectoryName(outFileName);
+                if (!Directory.Exists(outFileFolder))
+                    Directory.CreateDirectory(outFileFolder);
+
+                using (var inFile = new StreamReader(infileItem.ItemSpec, true))
+                using (var outFile = new StreamWriter(outFileName))
+                {
+                    var inputText = inFile.ReadToEnd();
+
+
+                    var result = Generate(inputText, namespaceName);
+
+                    outFile.WriteLine(result);
+                    outFile.Close();
+                }
+
+                resultFiles.Add(new TaskItem(outFileName));
+            }
+
+            ResultFiles = resultFiles.ToArray();
+
+            return true;
+        }
+
+        private string GetNamespace(string customToolNamespace, string relativePath, string rootNamespace)
+        {
+            if (!string.IsNullOrEmpty(customToolNamespace))
+                return customToolNamespace;
+
+            return $"{rootNamespace}.{relativePath.Replace(Path.DirectorySeparatorChar, '.')}".TrimEnd('.');
+
+        }
+    }
+}
+
---
+++ For_blog/Generators/Generators.Build/EntityGeneratorTask.cs
@@ -0,0 +1,23 @@
+﻿using Generators.Common.EntitiesModel;
+using Generators.Implementation.TemplateBased.T4;
+using Generators.ModelReaders;
+
+namespace Generators.Build
+{
+    public class EntityGeneratorTask : BaseGeneratorTask
+    {
+        protected override string OutFileExtension => ".cs";
+
+        protected override string Generate(string modelContent, string namspaceName)
+        {
+            var model = JsonModelReader.Read(modelContent);
+            var fullModel = new Model
+            {
+                Entities = model,
+                NamespaceName = namspaceName
+            };
+            var generator = new T4Generator();
+            return generator.Generate(fullModel);
+        }
+    }
+}
