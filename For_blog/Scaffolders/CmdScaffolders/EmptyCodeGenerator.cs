136ba9a393cd541e57863dd736e8c053381454d2
Mihail Romanov <romanov.m@skbkontur.ru>
2021-02-14 17:34:18 +0400

    Add empty console scaffolder

---
+++ For_blog/Scaffolders/CmdScaffolders/EmptyCodeGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CmdScaffolders
+{
+    public class EmptyCodeGenerator
+    {
+        public void GenerateCode(EmptyCodeGeneratorModel model)
+        {
+            Console.WriteLine("EmptyCodeGenerator !!!");
+        }
+    }
+
+    public class EmptyCodeGeneratorModel
+    {
+    }
+}
