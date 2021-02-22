8e670a46d710496cb6b42c65dd6469cfa756d634
Mihail Romanov <romanov.m@skbkontur.ru>
2021-02-22 18:32:31 +0400

    Add VS scaffolder

---
+++ For_blog/Scaffolders/VsScaffolders/DoNothing/DoNothingCodeGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Scaffolding;
+using System.Windows;
+
+namespace VsScaffolders.DoNothing
+{
+    public class DoNothingCodeGenerator : CodeGenerator
+    {
+        public DoNothingCodeGenerator(CodeGenerationContext context, CodeGeneratorInformation information)
+            : base(context, information)
+        {
+        }
+
+        public override void GenerateCode()
+        {
+            MessageBox.Show("Generate!!!");
+        }
+
+        public override bool ShowUIAndValidate()
+        {
+            return true;
+        }
+    }
+}
---
+++ For_blog/Scaffolders/VsScaffolders/DoNothing/DoNothingCodeGeneratorFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Scaffolding;
+using System;
+using System.ComponentModel.Composition;
+
+namespace VsScaffolders.DoNothing
+{
+    [Export(typeof(CodeGeneratorFactory))]
+    public class DoNothingCodeGeneratorFactory : CodeGeneratorFactory
+    {
+        public DoNothingCodeGeneratorFactory() :
+            base(new CodeGeneratorInformation(
+                "DoNothing Code Generator", "Generator that doesn't do anything",
+                "Some authors", new Version(1, 0, 0), nameof(DoNothingCodeGenerator)))
+        {
+        }
+
+        public override ICodeGenerator CreateInstance(CodeGenerationContext context)
+        {
+            return new DoNothingCodeGenerator(context, Information);
+        }
+    }
+}
