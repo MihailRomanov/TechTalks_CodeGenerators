2e5d5adb4207ff023dca747de6e81830b0549f53
Mihail Romanov <romanov.m@skbkontur.ru>
2021-02-27 21:00:20 +0400

    Add simple generator

---
+++ For_blog/Scaffolders/VsScaffolders/Simple/SimpleCodeGenerator.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Scaffolding;
+using Microsoft.VisualStudio.PlatformUI;
+using System.Windows;
+
+namespace VsScaffolders.Simple
+{
+    public class SimpleCodeGenerator : CodeGenerator
+    {
+        public SimpleCodeGenerator(CodeGenerationContext context, CodeGeneratorInformation information)
+            : base(context, information)
+        {
+        }
+
+        public override void GenerateCode()
+        {
+            MessageBox.Show("Generate!!!");
+        }
+
+        public override bool ShowUIAndValidate()
+        {
+            var dialog = new Dialog();
+            var result = dialog.ShowDialog();
+
+            return result ?? false;
+        }
+    }
+}
