3492a63608f26ce13cafd35ddf747caa42edb775
Mihail Romanov <romanov.m@skbkontur.ru>
2021-03-07 11:05:56 +0400

    Change listbox to xcseed control

--- For_blog/Scaffolders/VsScaffolders/UI/SelectCodeModelDialog.xaml.cs
+++ For_blog/Scaffolders/VsScaffolders/UI/SelectCodeModelDialog.xaml.cs
@@ -1,18 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using VsScaffolders.Model;
+using Xceed.Wpf.Toolkit;
+using Xceed.Wpf.Toolkit.Primitives;
 
 namespace VsScaffolders.UI
 {
@@ -26,15 +16,15 @@ namespace VsScaffolders.UI
             InitializeComponent();
         }
 
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            var selectedItems = ((ListBox)sender).SelectedItems;
-            ((SelectCodeModelViewModel)DataContext).SelectedProperties = selectedItems.Cast<CodePropertyModel>().ToArray();
-        }
-
         private void ОкButton_Click(object sender, RoutedEventArgs e)
         {
             DialogResult = true;
+        }
+
+        private void modelProperties_ItemSelectionChanged(object sender, ItemSelectionChangedEventArgs e)
+        {
+            var selectedItems = ((CheckListBox)sender).SelectedItems;
+            ((SelectCodeModelViewModel)DataContext).SelectedProperties = selectedItems.Cast<CodePropertyModel>().ToArray();
         }
     }
 }
